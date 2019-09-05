8ac9447f045b2daf36c012cb4c46e285daa5cf93
roopevi <roope.vilo@nimbledevices.com>
2019-09-05 14:59:08 +0300

    moved steerpath-smart-sdk import from app.js to index-web.js

--- example/src/index.web.js
+++ example/src/index.web.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-undef */
 import { AppRegistry } from 'react-native';
 import App from './App';
+import 'steerpath-smart-sdk/steerpath-smart.css'
+
 /**
  * React native vector icons font setup
  */
