6cdc82d7aa5d69401ee68b26298fc3edde75291c
Jarvis Luong <jarvisluong@gmail.com>
2019-12-12 12:06:42 +0200

    allow dynamic config in iOS

--- ios/RNSmartMapManager.m
+++ ios/RNSmartMapManager.m
@@ -17,6 +17,18 @@ RCT_EXPORT_METHOD(start:(NSString *) apiKey)
   [[SPSmartSDK getInstance] start: apiKey];
 }
 
+RCT_EXPORT_METHOD(startWithConfig:(nonnull NSDictionary *)config)
+{
+    NSString* apiKey = [config objectForKey:@"apiKey"];
+    NSString* configFilePath = [config objectForKey:@"configFilePath"];
+    
+    if (!apiKey || !configFilePath || ![apiKey isKindOfClass:[NSString class]] || ![configFilePath isKindOfClass:[NSString class]]) {
+        return;
+    }
+    
+    [[SPSmartSDK getInstance] start:apiKey config:configFilePath];
+}
+
 RCT_EXPORT_METHOD(setLiveConfig:(NSDictionary *)config)
 {
     [[SPSmartSDK getInstance] setLiveConfiguration: config];
