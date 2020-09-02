dfc407a453d879b10cafb6c7fec6ee5c96ce7cf1
roopevi <roope.vilo@nimbledevices.com>
2020-09-02 11:59:59 +0300

    upgrade Android smart sdk version to 1.3.13.2 - update CHANGELOG

--- example/src/config.js
+++ example/src/config.js
@@ -1,257 +1,275 @@
 export const CONFIG_STRING = `{
-	"services": {
-		"settings": {
-			"default": {
-				"enabled": false,
-				"externalLinks": []
-			},
-			"web": {
-				"enabled": true,
-				"settingsButtons": [{
-						"title": "sp_set_user_location_title",
-						"description": "sp_set_user_location_description",
-						"action": {
-							"type": "set_user_location"
-						},
-						"type": "button"
-					},
-					{
-						"title": "sp_set_dark_style_title",
-						"description": "sp_set_dark_style_description",
-						"action": {
-							"type": "set_dark_style"
-						},
-						"type": "button"
-					},
-					{
-						"title": "sp_set_default_style_title",
-						"description": "sp_set_default_style_description",
-						"action": {
-							"type": "set_default_style"
-						},
-						"type": "button"
-					}
-				],
-				"externalLinks": [{
-						"title": "sp_send_feedback_title",
-						"link": "support@steerpath.com",
-						"type": "email"
-					},
-					{
-						"title": "sp_visit_website_title",
-						"link": "https://steerpath.com",
-						"type": "web"
-					}
-				]
-			}
-		},
-		"smartmap": {
-			"default": {
-				"initialSearchBottomSheetState": "collapsed",
-				"defaultLanguage": "en-GB",
-				"supportedLanguages": [
-					"en-GB",
-					"fi-FI"
-				],
-				"defaultTheme": "default",
-				"supportedThemes": [
-					"default",
-					"dark"
-				],
-				"useFloorBasedStyle": true,
-				"nonSelectableCssClasses": ["infrastructure_stairs", "infrastructure_lift", "category_stairs", "category_other", "infrastructure_hole", "category_cover_poi", "infrastructure_department", "infrastructure_wing"],
-				"mapDataURL": "https://mapdata.eu.steerpath.com/",
-				"bluedot": {
-					"indoor": true,
-					"outdoor": false
-				},
-				"routeDataURL": "https://routes.eu.steerpath.com/",
-				"navigationDestinationThresholdM": 5,
-				"navigationRerouteThresholdM": 5,
-				"mapStylePath": "/style/default_v2.json",
-				"urlServiceURL": "https://url.steerpath.com/",
-				"viewProperties": {
-					"bearing": -2.4,
-					"pitch": 0,
-					"layerIndex": 2,
-					"buildingRef": "431",
-					"bounds": {
-						"sw": {
-							"lng": 24.812049873812953,
-							"lat": 60.22079674581377
-						},
-						"ne": {
-							"lng": 24.812706546050777,
-							"lat": 60.221077322610995
-						}
-					}
-				}
-			}
-		},
-		"search": {
-			"default": {
-				"nonSearchableCssClasses": ["infrastructure_stairs", "infrastructure_lift", "category_stairs", "category_other", "infrastructure_hole", "category_cover_poi", "infrastructure_department", "infrastructure_wing"],
-				"searchSuggestions": [{
-						"title": "sp_show_free_desk_title",
-						"shortTitle": "sp_show_free_desk_title_short",
-						"description": "sp_show_free_desk_description",
-						"iconName": "service_desk_free",
-						"action": {
-							"type": "show_free_live_rooms",
-							"allTags": ["service_desk"]
-						}
-					},
-					{
-						"title": "sp_show_free_phone_booths_title",
-						"shortTitle": "sp_show_free_phone_booths_title_short",
-						"description": "sp_show_free_phone_booths_description",
-						"iconName": "service_phone_free",
-						"action": {
-							"type": "show_free_live_rooms",
-							"allTags": ["service_phone"]
-						}
-					},
-					{
-						"title": "sp_show_free_rooms_title",
-						"shortTitle": "sp_show_free_rooms_title_short",
-						"description": "sp_show_free_rooms_description",
-						"iconName": "category_meeting_room_free",
-						"action": {
-							"type": "show_free_live_rooms",
-							"anyTags": ["room", "category_room"]
-						}
-					},
-					{
-						"title": "sp_category_meeting_room_title",
-						"shortTitle": "sp_category_meeting_room_title_short",
-						"description": "sp_category_meeting_room_description",
-						"iconName": "category_meeting_room",
-						"action": {
-							"type": "show_pois_with_tags",
-							"allTags": [
-								"category_meeting_room"
-							]
-						}
-					},
-					{
-						"title": "sp_service_toilet_title",
-						"shortTitle": "sp_service_toilet_title_short",
-						"description": "sp_service_toilet_description",
-						"iconName": "service_toilet",
-						"action": {
-							"type": "show_pois_with_tags",
-							"anyTags": ["service_toilet", "toilet"]
-						}
-					}
-				],
-				"moreSuggestionsButton": {
-					"title": "sp_more_suggestions_title",
-					"shortTitle": "sp_more_suggestions_title_short",
-					"description": "sp_more_suggestions_description",
-					"iconName": "category_more",
-					"action": {
-						"type": "show_more_categories"
-					}
-				}
-			}
-		},
-		"kiosk": {
-			"share": {
-				"hash": false,
-				"qrCodeShare": {
-					"enabled": true,
-					"qrCodeURL": "https://kiosk.steerpath.com/",
-					"qrCodePath": "/steerpath/index.html"
-				},
-				"copyLinkToClipboard": true
-			}
-		},
-		"telemetry": {
-			"default": {
-				"telemetryURL": "https://capture-v1.eu.steerpath.com/v1/",
-				"beacons": "known",
-				"location": "indoor",
-				"enabled": false,
-				"locationIntervalS": 60,
-				"transmissionIntervalS": 120
-			},
-			"web": {
-				"enabled": false,
-				"telemetryURL": "https://capture-v1.eu.steerpath.com/v1/"
-			}
-		},
-		"metadata": {
-			"default": {
-				"metadataURL": "https://meta2.eu.steerpath.com/meta/v2/"
-			}
-		},
-		"offline": {
-			"default": {
-				"offlineDataURL": "https://offline.eu.steerpath.com/"
-			}
-		},
-		"live": {
-			"default": {
-				"liveURL": "https://live-eu3.steerpath.com/",
-				"liveApiKey": "eyJhbGciOiJSUzI1NiJ9.eyJpYXQ6IjoxNTY4MDI5MTc0LCJqdGkiOiJlNzY5N2NiMS0wYmE4LTRhNTUtOWY5Zi01OGM5NjNmMDFlZDMiLCJzY29wZXMiOiJ2Mi0zYWU0MTdiYS0wMzg3LTRlNzktYTUxOS03ZWU5MjE3NmRjNjgtbGl2ZTpyLHciLCJzdWIiOiJ2Mi0zYWU0MTdiYS0wMzg3LTRlNzktYTUxOS03ZWU5MjE3NmRjNjgifQ.DV2Vnt5AJfuPv1K8X3Rw0u1SOFROyrr1KHLc6T7cd9R4BwTSgxt69IMYcc4ROTgqP254F3XUh_d92ku0W86pDlJslAoLcqA1T4MMkSgEMFvTl0V3ykAcNQDDs2opmudr4acIbbCJbtYe7_CG4GKV7WAnVocjbXqexpmrbTUol8xWhtuV2uB1ZODGCNLd2morhJYmMJtzp1jugtyvYO8-JKO4clKd5GfeWZ9YL9OywsGxanU5HKdpYivmWsLxCUGw8RajoM9hu9_APHNyw8jj8WDGUwZNqGfvTNHK17jAuZhJS5BaaelJoZCURyn7QqJVPp6QW3y6QW44IE7_H8ol-g",
-				"enabled": true
-			},
-			"web": {
-				"enabled": true,
-				"liveApiKey": "eyJhbGciOiJSUzI1NiJ9.eyJpYXQ6IjoxNTY4MDI5MTc0LCJqdGkiOiJmOWYxMzlkMS03OWRhLTRiMWEtYjZkNS1iM2Y1MTUwMWRiMzMiLCJzY29wZXMiOiJ2Mi0zYWU0MTdiYS0wMzg3LTRlNzktYTUxOS03ZWU5MjE3NmRjNjgtbGl2ZTpyIiwic3ViIjoidjItM2FlNDE3YmEtMDM4Ny00ZTc5LWE1MTktN2VlOTIxNzZkYzY4In0.WzW22UAZkDMQnD8XDTVewnghareFvHnkZqf9oh4UPm1ZPPIYHbgZ73MQ5M2vRRXsLr5_rsduSt1D620Mwmy0fN6O92Ra_kMEX5NVJR1vZC2S8sUP8ech_bNOF7HEBmVNGyBhT_h3_kZpAAIfMirBimS3UStIutpY_kBbHye6g8_HH2lHbWnh2ZDN76lSu2pmGNMpxIo_TdP_gKfnillbKlo3HyC2ztttMuCnLqmMLcatfX2pGXhzN94yuPIfvKQJ8mtOR_HxkgggzUOlkzZ_lsQHDQsob_ynn44LypbKWn7hDaVRqYY1r95EJHpL1nBT0WD3qFGlA6-VksLWrKaY6Q",
-				"liveURL": "https://live-eu3.steerpath.com/sdk-data"
-			}
-		},
-		"positioning": {
-			"default": {
-				"beaconsURL": "https://beacons2.eu.steerpath.com/",
-				"nddURL": "https://ndd.eu.steerpath.com/",
-				"eidURL": "https://eidupdates.eu.steerpath.com/",
-				"useAccelerometer": true,
-				"useGyro": false,
-				"useCompass": false,
-				"gpsThresholdM": 10
-			},
-			"ios": {
-				"useCompass": true
-			}
-		},
-		"ui": {
-			"default": {
-				"colors": {
-					"primaryColor": "#01b2a5",
-
-					"bluedotColor": "#3887be",
-					"backgroundColor": "#fafafa",
-
-					"zoomedInRouteLineColor": "#01b2a5",
-					"zoomedOutRouteLineColor": "#d7b127",
-					"altRouteLineColor": "#DEC56B",
-
-					"backgroundColorLayout": "rgba(255, 255, 255, 0.6)",
-					"backgroundColorItem": "#fafafa",
-
-					"textColor": "#000",
-					"textHaloColor": "#fff"
-				},
-				"colors_dark": {
-					"primaryColor": "#272b3f",
-
-					"bluedotColor": "#3a5e78",
-					"backgroundColor": "#fafafa",
-
-					"zoomedInRouteLineColor": "#3a7874",
-					"zoomedOutRouteLineColor": "#d7b127",
-					"altRouteLineColor": "#DEC56B",
-
-					"backgroundColorLayout": "rgba(0, 0, 0, 0.9)",
-					"backgroundColorItem": "#333333",
-
-					"textColor": "#fff",
-					"textHaloColor": "#000"
-				}
-			}
-		}
-	},
-	"update": false
+  "services": {
+    "settings": {
+      "default": {
+        "enabled": true,
+        "externalLinks": []
+      },
+      "web": {
+        "enabled": true,
+        "settingsButtons": [
+          {
+            "title": "sp_set_user_location_title",
+            "description": "sp_set_user_location_description",
+            "action": {
+              "type": "set_user_location"
+            },
+            "type": "button"
+          },
+          {
+            "title": "sp_set_dark_style_title",
+            "description": "sp_set_dark_style_description",
+            "action": {
+              "type": "set_dark_style"
+            },
+            "type": "button"
+          },
+          {
+            "title": "sp_set_default_style_title",
+            "description": "sp_set_default_style_description",
+            "action": {
+              "type": "set_default_style"
+            },
+            "type": "button"
+          }
+        ],
+        "externalLinks": [
+          {
+            "title": "sp_send_feedback_title",
+            "link": "support@steerpath.com",
+            "type": "email"
+          },
+          {
+            "title": "sp_visit_website_title",
+            "link": "https://steerpath.com",
+            "type": "web"
+          }
+        ]
+      }
+    },
+    "smartmap": {
+      "default": {
+        "defaultTheme": "default",
+        "supportedThemes": [
+          "default",
+          "dark"
+        ],
+        "useFloorBasedStyle": true,
+        "nonSelectableCssClasses": [
+          "infrastructure_stairs",
+          "infrastructure_lift",
+          "category_stairs",
+          "category_other",
+          "infrastructure_hole",
+          "category_cover_poi",
+          "infrastructure_department",
+          "infrastructure_wing"
+        ],
+        "mapDataURL": "https://mapdata.eu.steerpath.net/",
+        "bluedot": {
+          "indoor": true,
+          "outdoor": false
+        },
+        "routeDataURL": "https://routes.eu.steerpath.net/",
+        "navigationDestinationThresholdM": 5,
+        "navigationRerouteThresholdM": 5,
+        "mapStylePath": "/style/default.json",
+        "urlServiceURL": "https://url.steerpath.com/",
+        "viewProperties": {
+          "bearing": -2.4,
+          "pitch": 0,
+          "layerIndex": 2,
+          "buildingRef": "431",
+          "bounds": {
+            "sw": {
+              "lng": 24.812049873812953,
+              "lat": 60.22079674581377
+            },
+            "ne": {
+              "lng": 24.812706546050777,
+              "lat": 60.221077322610995
+            }
+          }
+        }
+      }
+    },
+    "search": {
+      "default": {
+        "nonSearchableCssClasses": [
+          "infrastructure_stairs",
+          "infrastructure_lift",
+          "category_stairs",
+          "category_other",
+          "infrastructure_hole",
+          "category_cover_poi",
+          "infrastructure_department",
+          "infrastructure_wing"
+        ],
+        "searchSuggestions": [
+          {
+            "title": "sp_show_free_desk_title",
+            "shortTitle": "sp_show_free_desk_title_short",
+            "description": "sp_show_free_desk_description",
+            "iconName": "service_desk_free",
+            "action": {
+              "type": "show_free_live_rooms",
+              "allTags": [
+                "service_desk"
+              ]
+            }
+          },
+          {
+            "title": "sp_show_free_phone_booths_title",
+            "shortTitle": "sp_show_free_phone_booths_title_short",
+            "description": "sp_show_free_phone_booths_description",
+            "iconName": "service_phone_free",
+            "action": {
+              "type": "show_free_live_rooms",
+              "allTags": [
+                "service_phone"
+              ]
+            }
+          },
+          {
+            "title": "sp_show_free_rooms_title",
+            "shortTitle": "sp_show_free_rooms_title_short",
+            "description": "sp_show_free_rooms_description",
+            "iconName": "category_meeting_room_free",
+            "action": {
+              "type": "show_free_live_rooms",
+              "anyTags": [
+                "room",
+                "category_room"
+              ]
+            }
+          },
+          {
+            "title": "sp_category_meeting_room_title",
+            "shortTitle": "sp_category_meeting_room_title_short",
+            "description": "sp_category_meeting_room_description",
+            "iconName": "category_meeting_room",
+            "action": {
+              "type": "show_pois_with_tags",
+              "allTags": [
+                "category_meeting_room"
+              ]
+            }
+          },
+          {
+            "title": "sp_service_toilet_title",
+            "shortTitle": "sp_service_toilet_title_short",
+            "description": "sp_service_toilet_description",
+            "iconName": "service_toilet",
+            "action": {
+              "type": "show_pois_with_tags",
+              "anyTags": [
+                "service_toilet",
+                "toilet"
+              ]
+            }
+          }
+        ],
+        "moreSuggestionsButton": {
+          "title": "sp_more_suggestions_title",
+          "shortTitle": "sp_more_suggestions_title_short",
+          "description": "sp_more_suggestions_description",
+          "iconName": "category_more",
+          "action": {
+            "type": "show_more_categories"
+          }
+        }
+      }
+    },
+    "kiosk": {
+      "share": {
+        "hash": false,
+        "qrCodeShare": {
+          "enabled": true,
+          "qrCodeURL": "https://kiosk.steerpath.com/",
+          "qrCodePath": "/steerpath/index.html"
+        },
+        "copyLinkToClipboard": false
+      }
+    },
+    "telemetry": {
+      "default": {
+        "telemetryURL": "https://capture-v1.eu.steerpath.com/v1/",
+        "beacons": "known",
+        "location": "indoor",
+        "enabled": false,
+        "locationIntervalS": 60,
+        "transmissionIntervalS": 120
+      },
+      "web": {
+        "enabled": false,
+        "telemetryURL": "https://capture-v1.eu.steerpath.com/v1/"
+      }
+    },
+    "metadata": {
+      "default": {
+        "metadataURL": "https://meta2.eu.steerpath.net/meta/v2/"
+      }
+    },
+    "offline": {
+      "default": {
+        "offlineDataURL": "https://offline.eu.steerpath.net/"
+      }
+    },
+    "live": {
+      "default": {
+        "liveURL": "https://live-eu3.steerpath.net/",
+        "liveApiKey": "eyJhbGciOiJSUzI1NiJ9.eyJpYXQ6IjoxNTY4MDI5MTc0LCJqdGkiOiJlNzY5N2NiMS0wYmE4LTRhNTUtOWY5Zi01OGM5NjNmMDFlZDMiLCJzY29wZXMiOiJ2Mi0zYWU0MTdiYS0wMzg3LTRlNzktYTUxOS03ZWU5MjE3NmRjNjgtbGl2ZTpyLHciLCJzdWIiOiJ2Mi0zYWU0MTdiYS0wMzg3LTRlNzktYTUxOS03ZWU5MjE3NmRjNjgifQ.DV2Vnt5AJfuPv1K8X3Rw0u1SOFROyrr1KHLc6T7cd9R4BwTSgxt69IMYcc4ROTgqP254F3XUh_d92ku0W86pDlJslAoLcqA1T4MMkSgEMFvTl0V3ykAcNQDDs2opmudr4acIbbCJbtYe7_CG4GKV7WAnVocjbXqexpmrbTUol8xWhtuV2uB1ZODGCNLd2morhJYmMJtzp1jugtyvYO8-JKO4clKd5GfeWZ9YL9OywsGxanU5HKdpYivmWsLxCUGw8RajoM9hu9_APHNyw8jj8WDGUwZNqGfvTNHK17jAuZhJS5BaaelJoZCURyn7QqJVPp6QW3y6QW44IE7_H8ol-g",
+        "enabled": true
+      },
+      "web": {
+        "enabled": true,
+        "liveApiKey": "eyJhbGciOiJSUzI1NiJ9.eyJpYXQ6IjoxNTY4MDI5MTc0LCJqdGkiOiJmOWYxMzlkMS03OWRhLTRiMWEtYjZkNS1iM2Y1MTUwMWRiMzMiLCJzY29wZXMiOiJ2Mi0zYWU0MTdiYS0wMzg3LTRlNzktYTUxOS03ZWU5MjE3NmRjNjgtbGl2ZTpyIiwic3ViIjoidjItM2FlNDE3YmEtMDM4Ny00ZTc5LWE1MTktN2VlOTIxNzZkYzY4In0.WzW22UAZkDMQnD8XDTVewnghareFvHnkZqf9oh4UPm1ZPPIYHbgZ73MQ5M2vRRXsLr5_rsduSt1D620Mwmy0fN6O92Ra_kMEX5NVJR1vZC2S8sUP8ech_bNOF7HEBmVNGyBhT_h3_kZpAAIfMirBimS3UStIutpY_kBbHye6g8_HH2lHbWnh2ZDN76lSu2pmGNMpxIo_TdP_gKfnillbKlo3HyC2ztttMuCnLqmMLcatfX2pGXhzN94yuPIfvKQJ8mtOR_HxkgggzUOlkzZ_lsQHDQsob_ynn44LypbKWn7hDaVRqYY1r95EJHpL1nBT0WD3qFGlA6-VksLWrKaY6Q",
+        "liveURL": "https://live-eu3.steerpath.net/sdk-data"
+      }
+    },
+    "positioning": {
+      "default": {
+        "beaconsURL": "https://beacons2.eu.steerpath.net/",
+        "nddURL": "https://ndd.eu.steerpath.net/",
+        "eidURL": "https://eidupdates.eu.steerpath.net/",
+        "useAccelerometer": true,
+        "useGyro": false,
+        "useCompass": false,
+        "gpsThresholdM": 10
+      },
+      "ios": {
+        "useCompass": true
+      }
+    },
+    "ui": {
+      "default": {
+        "colors": {
+          "primaryColor": "#857CE8",
+          "secondaryColor": "#857CE8",
+          "bluedotColor": "#3887be",
+          "backgroundColor": "#fafafa",
+          "zoomedInRouteLineColor": "#01b2a5",
+          "zoomedOutRouteLineColor": "#d7b127",
+          "altRouteLineColor": "#DEC56B",
+          "backgroundColorLayout": "rgba(255, 255, 255, 0.6)",
+          "backgroundColorItem": "#fafafa",
+          "textColor": "#000",
+          "textHaloColor": "#fff"
+        },
+        "colors_dark": {
+          "primaryColor": "#272b3f",
+          "bluedotColor": "#3a5e78",
+          "backgroundColor": "#fafafa",
+          "zoomedInRouteLineColor": "#3a7874",
+          "zoomedOutRouteLineColor": "#d7b127",
+          "altRouteLineColor": "#DEC56B",
+          "backgroundColorLayout": "rgba(0, 0, 0, 0.9)",
+          "backgroundColorItem": "#333333",
+          "textColor": "#fff",
+          "textHaloColor": "#000"
+        }
+      }
+    }
+  },
+  "update": false
 }`;
